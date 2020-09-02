12ff3c5ea48bdf967cf0ece8562ca31a185cc317
Alexandr <colmoon@ya.ru>
2020-09-02 20:58:23 +0300

    add email and password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,23 +26,37 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    required: true,
+    validate: (email) => validator.isEmail(email),
     unique: true,
-    validate: {
-      validator(email) {
-        return validator.isEmail(email);
-      },
-      required: true,
-    },
   },
   password: {
     type: String,
     required: true,
-    validate: /[A-Za-z0-9\W]+/,
     minlength: 8,
+    validate: /[A-Za-z0-9]+/,
+    select: false,
   },
 },
 {
   versionKey: false,
 });
+
+// eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matcher) => {
+          if (!matcher) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
