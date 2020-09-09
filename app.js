f06e90f3704c26eb31f33ce1b054a9e9020af793
Alexandr <colmoon@ya.ru>
2020-09-09 14:04:42 +0300

    add crash-test

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
