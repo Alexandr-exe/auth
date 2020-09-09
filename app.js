05031eafddb58befaded9dcacecee83b271a8645
Alexandr <colmoon@ya.ru>
2020-09-09 11:32:20 +0300

    add celebrate, linkValid, errors handler

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const { celebrate, Joi } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
 const users = require('./routers/users');
 const cards = require('./routers/cards');
+const NotFoundError = require('./errors/NotFoundError');
+const linkValidator = require('./helpers/linkValidation');
 
 const app = express();
 
@@ -32,15 +35,52 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(requestLogger);
 
-app.post('/signup', createUser);
-app.post('/signin', login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+    avatar: Joi.string().custom(linkValidator).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().trim().min(5).required(),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().trim().min(5).required(),
+  }),
+}), login);
 
 app.use(auth);
 
 app.use('/users', users);
 app.use('/cards', cards);
+
 app.use((req, res) => {
-  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+  throw new NotFoundError('Запрашиваемый ресурс не найден');
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message } = err;
+
+  if (err.code === 11000) {
+    statusCode = 409;
+    message = 'Пользователь с таким email уже есть';
+  }
+
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    message = 'Запрос неверно сформирован';
+  }
+
+  res.status(statusCode).send({
+    message: statusCode === 500
+      ? 'На сервере произошла ошибка'
+      : message,
+  });
+
+  next();
 });
 
 app.use(errorLogger);
