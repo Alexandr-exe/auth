7d25654226abf73855d37e989c3af97fa234ea21
Alexandr <colmoon@ya.ru>
2020-09-09 13:59:34 +0300

    Add error signin

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const SinginError = require('../errors/SinginError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -46,12 +47,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        throw SinginError('Неправильные почта или пароль');
       }
       return bcrypt.compare(password, user.password)
         .then((matcher) => {
           if (!matcher) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            throw SinginError('Неправильные почта или пароль');
           }
           return user;
         });
