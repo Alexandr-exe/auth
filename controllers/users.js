b74b6bf87df8b40375e55a866f0f3c6eb71815f1
Alexandr <colmoon@ya.ru>
2020-09-09 16:44:16 +0300

    fix login

--- controllers/users.js
+++ controllers/users.js
@@ -76,8 +76,7 @@ const login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: true,
-        })
-        .end();
+        });
       res.send({
         token,
       });
