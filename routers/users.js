39b52a73098180b0d2b002a93ae45f8f7be04b44
Alexandr <colmoon@ya.ru>
2020-09-11 13:25:49 +0300

    fix error 401

--- routers/users.js
+++ routers/users.js
@@ -3,6 +3,7 @@ const { celebrate, Joi } = require('celebrate');
 const { getUsers, findUser } = require('../controllers/users');
 
 router.get('/', getUsers);
+
 router.get('/:userId', celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().length(24),
