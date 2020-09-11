1891638f047a9404c96f0c26471cff6755d6b557
Alexandr <colmoon@ya.ru>
2020-09-11 11:37:53 +0300

    add celebrate

--- routers/cards.js
+++ routers/cards.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const linkValidator = require('../helpers/linkValidation');
 const { getCard, deleteCard, createCard } = require('../controllers/cards');
 
 router.get('/', getCard);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().custom(linkValidator).required(),
+  }),
+}), createCard);
+
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
 
 module.exports = router;
